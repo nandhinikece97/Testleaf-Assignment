255b77f3c85e31097a62ab533ac2e20a0744665f
nandhinikece97 <nandhinikece97@gmail.com>
2025-07-23 13:07:02 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,33 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+
+		// version of type float with a value of 91.0f
+
+		float version = 91.0f;
+		System.out.println(version);
+
+		// developer of type String with a value of "Google"
+
+		String developer = "Google";
+		System.out.println(developer);
+
+		// isBeta of type boolean with a value of false
+
+		boolean isBeta = false;
+		System.out.println(isBeta);
+
+		// releaseYear of type int with a value of 2008
+
+		int releaseYear = 2008;
+		System.out.println(releaseYear);
+
+		// shortcutKey of type char with a value of 'C'
+
+		char shortcutKey = 'C';
+		System.out.println(shortcutKey);
+	}
+
+}
